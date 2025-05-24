9b3ca03beaebaeea700796638920dd25b03d2c59
kae-mi <rudahyang7815@gmail.com>
2025-05-24 17:40:30 +0900

    cicd test7

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +11,6 @@ public class HomeController {
         return "cicd-test";
     }
 
-    // newsdfsdfsdfsdfsdf
+    // newsdfsdfsdfsdfsdfsdf
 
 }
